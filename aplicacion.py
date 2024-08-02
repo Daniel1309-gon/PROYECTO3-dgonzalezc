07f633ca4cb44191f62549c61ea89365f1caf163
Daniel1309-gon <dgonzalezca@unal.edu.co>
2024-08-02 15:21:59 -0500

    cambio mysqlclient por pymysql

--- aplicacion.py
+++ aplicacion.py
@@ -2,6 +2,9 @@ from flask import Flask
 from db import db
 from flask_restful import Api
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("HOST_DB")}/{os.getenv("SCHEMA_DB")}'
